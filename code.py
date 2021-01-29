d7d69925dc1abf6ca2716e0bf96b82c1d0e53dcc
https://github.com/DaveLampton <298615+DaveLampton@users.noreply.github.com>
2021-01-28 18:49:35 -0800

    shows max value and cyclesOpen

--- code.py
+++ code.py
@@ -8,14 +8,21 @@ white background, a smaller black rectangle, and some white text.
 
 import board
 import busio
-import adafruit_lis3dh
+import digitalio
 import displayio
+import math
 import terminalio
 import time
+import adafruit_lis3dh
 from adafruit_display_text import label
 import adafruit_displayio_ssd1306
 
-i2c = busio.I2C(board.SCL, board.SDA)
+led = digitalio.DigitalInOut(board.D13)
+led.direction = digitalio.Direction.OUTPUT
+led.value = False
+
+#i2c = busio.I2C(board.SCL, board.SDA)
+i2c = board.I2C()
 lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x18)
 # Range can be RANGE_2_G, RANGE_4_G, RANGE_8_G or RANGE_16_G
 lis3dh.range = adafruit_lis3dh.RANGE_2_G
@@ -53,16 +60,40 @@ splash.append(inner_sprite)
 
 # Create the label
 text_area = label.Label(
-    terminalio.FONT, text=" "*15, color=0xFFFFFF, x=18, y=HEIGHT // 2 - 1
+    terminalio.FONT, text=" "*15, color=0xFFFFFF, x=20, y=HEIGHT // 2 - 1
 )
 splash.append(text_area)
+
+print('Program executing...')
+
+samples = [0, 0, 0, 0, 0]
+writeHead = 0
+threshold = 0.3
+max = 0
+cyclesOpen = 1
 
 while True:
     x, y, z = lis3dh.acceleration
     vertical = z - adafruit_lis3dh.STANDARD_GRAVITY
+    vertical = math.sqrt(math.pow(vertical, 2))
+    # average it over the last five samples
+    samples[writeHead] = vertical
+    average = round(sum(samples) / len(samples),2)
     # Update the label
-    if vertical > 0:
-        text_area.text = "Z =  %0.2f m/s^2" % vertical
+    text_area.text = "Z = %0.2f m/s^2" % average
+    if average > threshold:
+        led.value = True
+        if average > max:
+            max = average
+            print("Max: %0.2f" % max)
+        print("Cycles open: %i" % cyclesOpen)
+        cyclesOpen += 1
     else:
-        text_area.text = "Z = %0.2f m/s^2" % vertical
-    time.sleep(0.0001)
+        led.value = False
+        max = 0
+        cyclesOpen = 1
+    if writeHead == 4:
+        writeHead = 0
+    else:
+        writeHead += 1
+    time.sleep(0.00095)
